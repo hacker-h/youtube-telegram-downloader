23fdaac4ec7051657541dd52c70db992344d71ea
Henning Häcker <henning.haecker+github.com@gmail.com>
2020-10-18 16:54:25 +0200

    implement google drive with md5 check

--- backends/google_drive.py
+++ backends/google_drive.py
@@ -7,6 +7,14 @@ import os
 from backends.storage_interface import StorageInterface
 
 
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
 class GoogleDriveStorage(StorageInterface):
 
     def __init__(self, path="testDir"):
@@ -19,18 +27,15 @@ class GoogleDriveStorage(StorageInterface):
         self.drive = GoogleDrive(gauth)
 
     def file_exists(self, filename, parent_folder_id):
-        existing_id = None
-        existing_md5 = None
         file_list = self.drive.ListFile({'q': 'trashed=false'}).GetList()
+        file = None
         for file in file_list:
             parents = file['parents'][0]
             parent_id = parents['id']
             if parent_id == parent_folder_id:
                 if file['title'] == filename:
-                    existing_id = file['id']
-                    existing_md5 = file['md5Checksum']
-                    break
-        return existing_id, existing_md5
+                    return file
+        return file
 
     def get_root_folder_id(self, parent_folder_name):
         file_list = self.drive.ListFile(
@@ -48,26 +53,32 @@ class GoogleDriveStorage(StorageInterface):
             folder_id = folder['id']
         return folder_id
 
-    def upload(self, filename):
+    def upload(self, local_file_name):
 
         dir_name = 'testDir'
-        # filename = 'requirements.txt'
+        # file_name = 'requirements.txt'
 
         # check whether the file already exists
         folder_id = self.get_root_folder_id(dir_name)
 
-        file_id, file_md5 = self.file_exists(filename, folder_id)
-        if file_id is None:
+        file = self.file_exists(local_file_name, folder_id)
+        if file is None:
             print("uploading new file")
             f = self.drive.CreateFile(
                 {"parents": [{"kind": "drive#fileLink", "id": folder_id}]})
-            f.SetContentFile(filename)
+            f.SetContentFile(local_file_name)
             f.Upload()
         else:
             # file already exists
-            # TODO check if md5sums differ => update the remote file
+            file_md5 = file['md5Checksum']
 
-            # TODO update remote file instead of uploading a copy
-            print("updating existing file")
-            print(file_id)
-            print(file_md5)
+            # check if md5sums differ => update the remote file
+            local_md5 = md5(local_file_name)
+            if file_md5 == local_md5:
+                print("file is already up to date")
+            else:
+                print("updating existing file..")
+                # update remote file instead of uploading a copy
+                file.SetContentFile(local_file_name)
+                file.Upload()
+                print("remote file updated")
